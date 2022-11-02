cec00d20c5ccb26440afce5f5125598c3adfeaaa
Tima <dadeev.tim@gmail.com>
2022-11-02 15:36:29 +0700

    feat: fully implement lyrics project

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -81,36 +81,37 @@ const TopPlay = () => {
         </div>
       </div>
 
-      <div className="w-full flex flex-col mt-8">
-        <div className="flex flex-row justify-between items-center">
-          <h2 className="text-white font-bold text-2xl">Top Artists</h2>
-          <Link to="/top-artists">
-            <p className="text-gray-300 text-base cursor-pointer">See more</p>
-          </Link>
-        </div>
+      {/* technical works / fixes */}
+      {/* <div className="w-full flex flex-col mt-8"> */}
+      {/*  <div className="flex flex-row justify-between items-center"> */}
+      {/*    <h2 className="text-white font-bold text-2xl">Top Artists</h2> */}
+      {/*    <Link to="/top-artists"> */}
+      {/*      <p className="text-gray-300 text-base cursor-pointer">See more</p> */}
+      {/*    </Link> */}
+      {/*  </div> */}
 
-        <Swiper
-          slidesPerView="auto"
-          spaceBetween={15}
-          freeMode
-          centeredSlides
-          centeredSlidesBounds
-          modules={[FreeMode]}
-          className="mt-4"
-        >
-          {topPlays?.slice(0, 5).map((artist) => (
-            <SwiperSlide
-              key={artist?.key}
-              style={{ width: '25%', height: 'auto' }}
-              className="shadow-lg rounded-full animate-slideright"
-            >
-              <Link to={`/artists/${artist?.artists[0].adamid}`}>
-                <img src={artist?.images?.background} alt="Name" className="rounded-full w-full object-cover" />
-              </Link>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
+      {/*  <Swiper */}
+      {/*    slidesPerView="auto" */}
+      {/*    spaceBetween={15} */}
+      {/*    freeMode */}
+      {/*    centeredSlides */}
+      {/*    centeredSlidesBounds */}
+      {/*    modules={[FreeMode]} */}
+      {/*    className="mt-4" */}
+      {/*  > */}
+      {/*    {topPlays?.slice(0, 5).map((artist) => ( */}
+      {/*      <SwiperSlide */}
+      {/*        key={artist?.key} */}
+      {/*        style={{ width: '25%', height: 'auto' }} */}
+      {/*        className="shadow-lg rounded-full animate-slideright" */}
+      {/*      > */}
+      {/*        <Link to={`/artists/${artist?.artists[0].adamid}`}> */}
+      {/*          <img src={artist?.images?.background} alt="Name" className="rounded-full w-full object-cover" /> */}
+      {/*        </Link> */}
+      {/*      </SwiperSlide> */}
+      {/*    ))} */}
+      {/*  </Swiper> */}
+      {/* </div> */}
     </div>
   );
 };
