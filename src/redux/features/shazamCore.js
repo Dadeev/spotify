0e840af1993d68004999d526451b11a7d2856a06
Tima <dadeev.tim@gmail.com>
2022-11-02 15:19:59 +0700

    feat: completely implement search

--- src/redux/features/shazamCore.js
+++ src/redux/features/shazamCore.js
@@ -16,6 +16,7 @@ export const shazamCoreApi = createApi({
     getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
     getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
     getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
+    getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
   }),
 });
 
@@ -26,4 +27,5 @@ export const
     useGetSongRelatedQuery,
     useGetArtistDetailsQuery,
     useGetSongsByCountryQuery,
+    useGetSongsBySearchQuery,
   } = shazamCoreApi;
